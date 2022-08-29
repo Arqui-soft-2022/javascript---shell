68903ebf15f9f579da790c89e5a1ce1745622aa4
Brayan <tronks_465@hotmail.com>
2022-08-29 15:48:19 -0500

    Creacion de carpeta controller/ creacion auth controller

---
+++ controller/auth.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import 'colors';
+
+const register = async ( user ) => {
+    try {
+        const { data : { usuario }} = await axios.post('https://codeqr-generate.herokuapp.com/api/auth/register', user );
+        const { username, email, name } = usuario;
+        return { username, email, name };
+    } catch (error) {
+        let messageError = error?.response?.data?.errors?.shift().msg;
+        if( !messageError ) {
+            messageError = error?.response?.data?.msg;
+        } 
+         
+        console.log(`${'Errror: '.red } ${messageError}`);
+        return null;
+    }
+}
+
+const login = async ( username, password ) => {
+    try {
+        const { data : { usuario }} = await axios.post('https://codeqr-generate.herokuapp.com/api/auth/login', { username, password } );
+        return usuario;
+    } catch (error) {
+        let messageError = error?.response?.data?.errors?.shift().msg;
+        if( !messageError ) {
+            messageError = error?.response?.data?.msg;
+        } 
+         
+        console.log(`${'Errror: '.red } ${messageError}`);
+        return null;
+    }
+}
+
+export {register, login};
